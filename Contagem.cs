3a942fa79aa5824c050248406e7de572ebe05814
yurihventura <yuri_ven@hotmail.com>
2024-07-21 14:46:06 -0300

    contagem

---
+++ Contagem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program{
+    public static void Main (string [] args){
+
+        for(int i=0; i<=80; i++){
+            if (i % 5 == 0){
+                Console.WriteLine("valor: " + i);
+            }
+        }
+        
+    }
+    }
